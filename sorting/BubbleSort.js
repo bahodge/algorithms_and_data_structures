0bb5391fc77144170a5f422ee6c503732cbd8dd5
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2019-05-30 21:19:20 -0500

    selection sort

--- sorting/BubbleSort.js
+++ sorting/BubbleSort.js
@@ -1,19 +1,29 @@
 "use strict";
 
-const array = [1, 60, 3, 35, 20];
+const array1 = [1, 60, 3, 35, 20];
+const array2 = [2, 32, 6, 35, 29, 16, 78, 332, 325, 122, 5, 56, 6, 15, 267];
 
 const bubbleSort = arr => {
+  let noSwaps;
+  let stepCount = 1;
   for (let i = 0; i < arr.length; i++) {
+    noSwaps = true;
     for (var j = 0; j < arr.length; j++) {
+      console.log(`Iterations: ${stepCount}`);
       console.log(arr);
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+        noSwaps = false;
       }
+      stepCount++;
     }
+    if (noSwaps) break;
   }
   return arr;
 };
 
-console.log(bubbleSort(array));
+console.log(bubbleSort(array1));
+console.log("Done");
+console.log(bubbleSort(array2));
---
+++ sorting/SelectionSort.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const array1 = [5, 3, 4, 1, 2];
+
+const selectionSort = arr => {
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]) {
+        lowest = j;
+      }
+    }
+    if (i !== lowest) {
+      let temp = arr[i];
+      arr[i] = arr[lowest];
+      arr[lowest] = temp;
+    }
+  }
+
+  return arr;
+};
+
+console.log(selectionSort(array1));
