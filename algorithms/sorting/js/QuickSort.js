1b155aff289118c7fc1c06a45d42604c8b325aa0
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2019-09-29 10:40:17 -0500

    working

--- algorithms/sorting/js/QuickSort.js
+++ algorithms/sorting/js/QuickSort.js
@@ -1,39 +1,38 @@
 const sort = (array, left = 0, right = array.length - 1) => {
-  if (left < right) {
-    let pivotIdx = pivotHelper(array, left, right);
-    sort(array, left, pivotIdx - 1);
-    sort(array, pivotIdx + 1, right);
-  }
-  return array;
+    if (left < right) {
+        let pivotIdx = pivotHelper(array, left, right);
+        sort(array, left, pivotIdx - 1);
+        sort(array, pivotIdx + 1, right);
+    }
+    return array;
 };
 
 const pivotHelper = (array, startIdx = 0, endIdx = array.length + 1) => {
-  let pivot = array[startIdx];
-  let swapIdx = startIdx;
+    let pivot = array[startIdx];
+    let swapIdx = startIdx;
 
-  for (let i = startIdx + 1; i <= endIdx; i++) {
-    if (pivot > array[i]) {
-      swapIdx++;
-      swapItems(array, swapIdx, i);
+    for (let i = startIdx + 1; i <= endIdx; i++) {
+        if (pivot > array[i]) {
+            swapIdx++;
+            swapItems(array, swapIdx, i);
+        }
     }
-  }
-  swapItems(array, swapIdx, startIdx);
-  //   console.log(array);
-  return swapIdx;
+    swapItems(array, swapIdx, startIdx);
+    return swapIdx;
 };
 
 const swapItems = (array, idx1, idx2) => {
-  let tmp = array[idx1];
-  array[idx1] = array[idx2];
-  array[idx2] = tmp;
+    let tmp = array[idx1];
+    array[idx1] = array[idx2];
+    array[idx2] = tmp;
 };
 
 const array1 = Array.from({ length: 10 }, () =>
-  Math.floor(Math.random() * 100)
+    Math.floor(Math.random() * 100)
 );
 
 const array2 = Array.from({ length: 15 }, () =>
-  Math.floor(Math.random() * 100)
+    Math.floor(Math.random() * 100)
 );
 
 const array3 = [1, 60, 3, 35, 20];
